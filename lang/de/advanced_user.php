4723b27b3ebae64ff51c80e3dcc39e69a049b3fe
cdaviau <christian.daviau@recia.fr>
2017-03-08 16:28:55 +0100

    Ajout de la langue allemande

---
+++ lang/de/advanced_user.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Les message vont toujours par deux :
+ * - un lorsqu'on est un utilisateur standard
+ * - un lorsqu'on est un utilisateur avancé
+ *
+ * Exemple :
+ * le message ayant le code 'changer_role'
+ * Utilisateur standard : code 'changer_role-standard' => 'Switch to advanced user'
+ * Utilisateur avancé : code 'changer_role-avance' => 'Switch to standard user'
+ */
+$string['changer_role-standard']        = 'Auf Standardbenutzer umschalten';
+$string['changer_role-avance']          = 'Umschalten auf fortgeschrittenen Benutzer';
+$string['confirmer_role-standard']      = 'Sie wechseln zu fortgeschrittenem Benutzer';
+$string['confirmer_role-avance']        = 'Sie wechseln zum Standardbenutzer';
+?>
